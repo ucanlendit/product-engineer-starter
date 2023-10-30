e4f5ff6a48aeef57d397e1bd1ac26fd0cb32f5e6
ucanlendit <oleksander.bondarev@gmail.com>
2023-10-30 17:13:04 +0000

    build error fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,38 +1,13 @@
 import "@/styles/globals.css";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
-
-export const metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"  style={{ background: "#fefcf3" }} suppressHydrationWarning>
+    <html lang="en" style={{ background: "#fefcf3" }} suppressHydrationWarning>
       <head />
-      <body
-       
-        className={clsx(
-          "min-h-screen font-sans antialiased bg-haze-100",
-          fontSans.variable
-        )}>
+      <body className={clsx("min-h-screen font-sans antialiased bg-haze-100")}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
@@ -40,9 +15,7 @@ export default function RootLayout({ children }) {
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
-              <span
-                className="flex items-center gap-1 text-current"
-                >
+              <span className="flex items-center gap-1 text-current">
                 <span className="text-default-600">Powered by Co:Helm</span>
               </span>
             </footer>
